959f1674ad1905d03902882b28ac3141914f05dc
Cams <117851597+CamilaParedes23@users.noreply.github.com>
2025-11-06 20:27:02 -0500

    Normalize line endings: core.autocrlf=true

--- src/test/java/espe/edu/tienda_ropa/TiendaRopaApplicationTests.java
+++ src/test/java/espe/edu/tienda_ropa/TiendaRopaApplicationTests.java
@@ -2,7 +2,9 @@ package espe.edu.tienda_ropa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TiendaRopaApplicationTests {
 
